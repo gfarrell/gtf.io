d9a1eb7d015e743502689980930bb629e6dd1974
Gideon Farrell <gideon@gtf.io>
2024-03-07 22:50:54 +0000

    create a navbar partial

---
+++ src/GTF/Pages/Partials/Nav.hs
@@ -0,0 +1,31 @@
+module GTF.Pages.Partials.Nav (navbar) where
+
+import CommonPrelude
+import Data.ByteString (ByteString, isPrefixOf)
+import Data.Text (unwords)
+import Data.Text.Encoding (decodeUtf8)
+import GTF.URL (UrlPath (..))
+import Lucid.Base (Html, toHtml)
+import Lucid.Html5
+
+navbar :: UrlPath -> Html ()
+navbar currentPath =
+  nav_ $
+    foldMap
+      mkMenuItem
+      [ ("home", "/"),
+        ("musings", "/musings"),
+        ("projects", "/projects"),
+        ("codices", "/codices"),
+        ("colophon", "/colophon")
+      ]
+  where
+    mkMenuItem :: (Text, ByteString) -> Html ()
+    mkMenuItem (label, url) =
+      span_ [class_ $ mkClass url] $
+        a_ [href_ . decodeUtf8 $ url] (toHtml label)
+
+    mkClass :: ByteString -> Text
+    mkClass url =
+      Data.Text.unwords $
+        "nav-item" : ["active" | url `isPrefixOf` unUrlPath currentPath && url /= "/"]
