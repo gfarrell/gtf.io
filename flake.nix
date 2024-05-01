46ef957f63ef02815c78a65601e7f4c09ebd07c1
Gideon Farrell <gideon@gtf.io>
2024-05-01 13:33:04 +0100

    fixed nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "gtf.io website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     hs-flake-utils.url = "git+https://whetstone.private.storage/jcalderone/hs-flake-utils.git?ref=main";
     hs-flake-utils.inputs.nixpkgs.follows = "nixpkgs";
@@ -16,6 +16,7 @@
   }: let
     ulib = flake-utils.lib;
     ghcVersion = "ghc947";
+    # ^ `nix-env -f "<nixpkgs>" -qaP -A haskell.compiler` suggests we should have 947 but nix is complaining
   in
     ulib.eachSystem ["x86_64-linux"] (system: let
       pkgs = import nixpkgs {
@@ -23,9 +24,15 @@
       };
       hslib = hs-flake-utils.lib {
         inherit pkgs;
-        src = "./.";
+        src = ./.;
         compilerVersion = ghcVersion;
         packageName = "gtf-website";
+        hsPkgsOverrides = hfinal: hprev: {
+          # https://ryantm.github.io/nixpkgs/languages-frameworks/haskell/#haskell-derivation-args
+          djot = hfinal.mkDerivation {
+            src = "https://github.com/gfarrell/djoths.git";
+          };
+        };
       };
     in {
       checks = hslib.checks {};
@@ -34,8 +41,8 @@
           with pkgs; [
             zlib
           ];
-        };
-        packages = hslib.packages {};
-        apps.release = hslib.apps.release {};
+      };
+      packages = hslib.packages {};
+      apps.release = hslib.apps.release {};
     });
 }
