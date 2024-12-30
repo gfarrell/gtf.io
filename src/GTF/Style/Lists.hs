3be6029c0baa84ccce4386c649182e31ad470696
Gideon Farrell <gideon@gtf.io>
2024-12-30 17:30:35 +0000

    style definition lists

--- src/GTF/Style/Lists.hs
+++ src/GTF/Style/Lists.hs
@@ -1,6 +1,7 @@
 module GTF.Style.Lists (stylesheet) where
 
 import Clay
+import Clay.Flexbox qualified as FB
 import CommonPrelude ((<>))
 
 stylesheet :: Css
@@ -28,3 +29,23 @@ stylesheet = do
 
       ol ? do
         listStyleType lowerRoman
+
+  dl ? do
+    dt ? do
+      fontWeight bold
+
+  dl # ".dl-single-line" ? do
+    display flex
+    flexWrap FB.wrap
+
+    dt ? do
+      flexBasis (pct 20)
+      paddingRight (em 1.6)
+
+    dd ? do
+      flexBasis (pct 75)
+      marginLeft (em 0)
+
+      p ? margin (em 0) (em 0) (em 0) (em 0)
+
+    dd <> dt ? marginBottom (em 1)
